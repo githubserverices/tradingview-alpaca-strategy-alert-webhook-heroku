b9de9f7e9f55b85efd09140901d1954d7f288609
me <githubserverices>
2022-01-30 02:47:56 +0100

    config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY = 'youralpacaapikey'
-API_SECRET = 'youralpacaapisecret'
+API_KEY = 'jKylulUGX0BfSbLULwAOEG5qP40Cx7wHaclEFdScwFEiFLSvSfJtkad3dB2G0c4F'
+API_SECRET = 'GL34sTRHSZ2cYh6ZlWJrNu3oYEobGXwSmXjttFRXrpGLdLsqyK8ml72zpx1ONLyz'
 
-WEBHOOK_PASSPHRASE = 'yourpassphrase'
+WEBHOOK_PASSPHRASE = '5MQDh3UnhYmKa^2n6tu&EDeT44g3Hw&tu@qMd$'
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
